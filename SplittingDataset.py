7ee64324a4a96cb7d3d9a46d4fa21d532c958374
Nataša Radmilović <45151070+natasarad02@users.noreply.github.com>
2023-09-08 17:12:57 +0200

    Update SplittingDataset.py

--- SplittingDataset.py
+++ SplittingDataset.py
@@ -2,8 +2,8 @@ import os
 import shutil
 from sklearn.model_selection import train_test_split
 
-source = 'path to preprocessed files directory'
-output = 'path to preprocessed split directory'
+source = 'your path to preprocessed files directory'
+output = 'your path to preprocessed split directory'
 
 # Defining ratios
 train_ratio = 0.7
